4dd34af6321ccfa2c79aa51ffb8085fe7340284f
Volodymyr <klymenkodp@gmail.com>
2019-01-02 22:19:07 -0500

    Updates

---
+++ precache-manifest.4dc9b1325dd56470c7b56a54ca40b573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82ed820565c8d4cb9fe4",
+    "url": "/static/js/main.82ed8205.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "82ed820565c8d4cb9fe4",
+    "url": "/static/css/main.c734f06c.chunk.css"
+  },
+  {
+    "revision": "5e5f00f76b4fa671bcb2844068a68830",
+    "url": "/index.html"
+  }
+];
